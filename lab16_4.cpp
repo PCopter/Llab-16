7564f56264707dd53455f86c3d4bddeb8b934cc8
Copter <copter.phanuwat@gamil.com>
2022-01-17 21:06:38 +0700

    lab 16 ok

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,43 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &w,int &x,int &y,int &z){
+	int a[4];
+	int b[4];
+	int q = 0;
+	int p = 0;
+	int e = 0;
+	int r = 0;
+	a[0] = w;
+	a[1] = x;
+	a[2] = y;
+	a[3] = z;
+	q = rand()%4;
+	b[0]= a[q];
+	a[q]=0;
+
+		do{
+			p = rand()%4;
+		}while(a[p]==0);
+		b[1] = a[p];
+		a[p] = 0;
+
+		do{
+			e = rand()%4;
+		}while(a[e]==0);
+		b[2] = a[e];
+		a[e] = 0;
+
+		do{
+			r = rand()%4;
+		}while(a[r]==0);
+		b[3] = a[r];
+		
+
+		w=b[0];
+		x=b[1];
+		y=b[2];
+		z=b[3];	
+	
+}
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *,int *,int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,50 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);  
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+
+void shuffle(int *w,int *x,int *y,int *z){
+	int a[4];
+	int b[4];
+	int q = 0;
+	int p = 0;
+	int e = 0;
+	int r = 0;
+	a[0] = *w;
+	a[1] = *x;
+	a[2] = *y;
+	a[3] = *z;
+	q = rand()%4;
+	b[0]= a[q];
+	a[q]=0;
+
+		do{
+			p = rand()%4;
+		}while(a[p]==0);
+		b[1] = a[p];
+		a[p] = 0;
+
+		do{
+			e = rand()%4;
+		}while(a[e]==0);
+		b[2] = a[e];
+		a[e] = 0;
+
+		do{
+			r = rand()%4;
+		}while(a[r]==0);
+		b[3] = a[r];
+		
+
+		*w=b[0];
+		*x=b[1];
+		*y=b[2];
+		*z=b[3];	
+	
+}
